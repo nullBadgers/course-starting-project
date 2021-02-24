88d8b6127bd8bed712d237bc58aefafef0182e75
Mojo <12233836+nullBadgers@users.noreply.github.com>
2021-02-24 20:07:57 +0000

    Update app.ts

--- app.ts
+++ app.ts
@@ -8,6 +8,9 @@ if (typeof userInput === 'string') {
 }
 // this IF type check will check if the unknown type of userInput is string, best to use unknown over the 'any' type
 
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+// never - never to return any values
 
-// test
-
+generateError('An error has happened. :(', 500);
